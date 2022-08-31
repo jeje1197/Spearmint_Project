663872b2ea225af8d1f3e13927744f61333b7488
Joseph E <1jojorun@gmail.com>
2022-08-31 06:15:28 -0700

    Updated CSS

--- src/front-end/pages/about/About.js
+++ src/front-end/pages/about/About.js
@@ -14,10 +14,16 @@ function AboutBody() {
     return (
         <div className="About">
             <p>
-                The Spearmint Programming Language is a imperative, dynamically-typed
+                The Spearmint Programming Language is a high-level, imperative
                 programming language developed in 2022 by computer scientist Joseph Evans.
                 It was inspired by the JavaScript and Python languages as it derives some
-                of its syntactical and functional attributes from them.
+                of its syntactical and functional attributes from them, such as its 
+                dynamic-typing and evaluation.
+            </p>
+            <p>
+                This project features a full-stack web application with an online code editor
+                for users to write and test their code without the need to download the Spearmint 
+                development package
             </p>
         </div>
     )
